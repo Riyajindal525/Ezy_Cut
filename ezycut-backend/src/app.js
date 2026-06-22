536973c76b581a90a43760220bc861c6156a6372
Riya Jindal <ritikjindal970@gmail.com>
2026-06-22 16:18:13 +0530

    Update cors

--- ezycut-backend/src/app.js
+++ ezycut-backend/src/app.js
@@ -54,7 +54,8 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:5500",
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://ezy-cut.vercel.app"
     ],
     credentials: true,
   })
